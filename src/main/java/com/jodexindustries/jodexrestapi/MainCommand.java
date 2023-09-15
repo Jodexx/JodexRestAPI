c8df2584f33ec39990b8442ab7715f894bef4cc8
Jodex <59202575+Jodexx@users.noreply.github.com>
2023-09-15 19:23:11 +0300

    Update MainCommand.java

--- src/main/java/com/jodexindustries/jodexrestapi/MainCommand.java
+++ src/main/java/com/jodexindustries/jodexrestapi/MainCommand.java
@@ -10,10 +10,10 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
             sender.sendMessage("§6Jodex§bRestAPI §7by §c_Jodex__");
-            sender.sendMessage("§7/jodexra §astart §f- §7запустить webapi сервер");
-            sender.sendMessage("§7/jodexra §astop §f- §7остановить webapi сервер");
-            sender.sendMessage("§7/jodexra §aurl §f- §7показать ссылку на webapi сервер");
-            sender.sendMessage("§7/jodexra §areloadconfig §f- §7перезагрузить конфиг");
+            sender.sendMessage("§7/jodexra §astart §f- §7start webapi server");
+            sender.sendMessage("§7/jodexra §astop §f- §7stop webapi server");
+            sender.sendMessage("§7/jodexra §aurl §f- §7show link webapi server");
+            sender.sendMessage("§7/jodexra §areloadconfig §f- §7reload config");
             return true;
         } else {
             if (args[0].equalsIgnoreCase("start")) {
@@ -21,15 +21,15 @@ public class MainCommand implements CommandExecutor {
                     if (!Utils.isServerRunning(WebServer.server)) {
                         try {
                             WebServer.start();
-                            sender.sendMessage("§aWeb сервер успешно запущен!");
+                            sender.sendMessage("§aWeb server successful started!");
                         } catch (IllegalAccessError var6) {
                             var6.printStackTrace();
                         }
                     } else {
-                        sender.sendMessage("§cWeb сервер уже запущен!");
+                        sender.sendMessage("§cWeb already started!");
                     }
                 } else {
-                    sender.sendMessage("§cУ вас недостаточно прав!");
+                    sender.sendMessage("§cYou don't have permissions!");
                 }
             }
 
@@ -37,29 +37,29 @@ public class MainCommand implements CommandExecutor {
                 if (sender.hasPermission("jodexrestapi.stop")) {
                     if (Utils.isServerRunning(WebServer.server)) {
                         WebServer.stop();
-                        sender.sendMessage("§aWeb сервер успешно остановлен!");
+                        sender.sendMessage("§aWeb server successful stopped!");
                     } else {
-                        sender.sendMessage("§cWeb сервер уже остановлен!");
+                        sender.sendMessage("§cWeb server already stopped!");
                     }
                 } else {
-                    sender.sendMessage("§cУ вас недостаточно прав!");
+                    sender.sendMessage("§cYou don't have permissions!");
                 }
             }
 
             if (args[0].equalsIgnoreCase("url")) {
                 if (sender.hasPermission("jodexrestapi.url")) {
-                    Utils.sendMessage(sender, "&cWeb сервер запущен: \n&bhttp://" + Utils.getServerIP() + ":" + Main.getInstance().getConfig().getInt("webserver.port") + "/api/player/" + sender.getName());
+                    Utils.sendMessage(sender, "&cWeb server started: \n&bhttp://" + Utils.getServerIP() + ":" + Main.getInstance().getConfig().getInt("webserver.port") + "/api/player/" + sender.getName());
                 } else {
-                    sender.sendMessage("§cУ вас недостаточно прав!");
+                    sender.sendMessage("§cYou don't have permissions!");
                 }
             }
 
             if (args[0].equalsIgnoreCase("reloadconfig")) {
                 if (sender.hasPermission("jodexrestapi.reloadconfig")) {
-                    sender.sendMessage("§aКонфиг успешно перезагружен!");
+                    sender.sendMessage("§aConfig successful reloaded!");
                     Main.getInstance().reloadConfig();
                 } else {
-                    sender.sendMessage("§cУ вас недостаточно прав!");
+                    sender.sendMessage("§cYou don't have permissions!");
                 }
             }
 
